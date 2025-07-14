77c912002b01f4958c887d3529f070bb547c226b
Khayot Razzakov <hrazzoqov0002@gmail.com>
2025-07-15 00:12:52 +0500

    admin panel

--- main/admin.py
+++ main/admin.py
@@ -3,9 +3,11 @@ from .models import *
 
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('user', 'amount', 'source', 'date')
+    list_filter = ('user',)
 
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('user', 'amount', 'source', 'date')
+    list_filter = ('user',)
 
 admin.site.register(Income, IncomeAdmin)
 admin.site.register(Expense, ExpenseAdmin)
