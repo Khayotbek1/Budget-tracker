50a47ca5806c28a61805c88b54f4ee9e4fb2e52f
Khayot Razzakov <hrazzoqov0002@gmail.com>
2025-08-14 23:26:24 +0500

    commit

--- core/urls.py
+++ core/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('api/', include('main.urls')),
 
     #docs
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 urlpatterns += i18n_patterns(
