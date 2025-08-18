24320397e09be228bd09e025607179c14233f26f
Khayot Razzakov <hrazzoqov0002@gmail.com>
2025-08-18 23:31:04 +0500

    add admin panel in user balance

--- users/admin.py
+++ users/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import *
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name')
+    list_display = ('username', 'first_name', 'last_name', 'balance')
+
 
 
 admin.site.register(User, UserAdmin)
