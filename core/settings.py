3b926638e287c3395b9b5cc3f38fe92d7a942099
Khayot Razzakov <114242863+Khayotbek1@users.noreply.github.com>
2025-09-22 13:23:47 +0500

    collect static

--- core/settings.py
+++ core/settings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xo7dhbw0a-5(@t10w_2ala0*pko_eth-929u$e#a50%2c(n239'
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'jazzmin',
@@ -140,6 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if DEBUG:
+    STATIC_FILES_DIRS = [BASE_DIR / 'static']
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
